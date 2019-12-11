4008f93f2c7c88c603077fed3263545eaf6e17b8
leocjj <leocj@hotmail.com>
2019-12-11 03:06:27 +0000

    Task_13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: address of a pointer to a structure of type listint_t
+ * @idx: index of the list where the new node should be added. Index starts: 0
+ * @n: data for inserted node
+ *
+ * Return: the address of the new node, or NULL if it failed. If it is not
+ * possible to add the new node at index idx, do not add and return NULL.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	int i = 1;
+	listint_t *temp_node = *head, *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	while ((*temp_node).n < number)
+	{
+		if (temp_node->next != NULL && (*(temp_node->next)).n < number)
+			temp_node = temp_node->next;
+		else
+			break;
+		i++;
+	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+
+	if (number == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else if (temp_node)
+	{
+		new_node->next = temp_node->next;
+		temp_node->next = new_node;
+	}
+	return (new_node);
+}
