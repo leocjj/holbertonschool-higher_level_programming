627d3d4f60e896b958bf1b2527b8b67e878395b8
leojj <leocj@hotmail.com>
2019-12-17 17:45:24 -0500

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(my_string):
 
     copy_string = ''
-    for item in range(len(my_string) - 1):
+    for item in range(len(my_string)):
         if (my_string[item] != 'c') and (my_string[item] != 'C'):
             copy_string += my_string[item]
     return copy_string
