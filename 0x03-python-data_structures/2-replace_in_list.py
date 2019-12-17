0621548df8c61584548b728dd18e3095636a58bb
leojj <leocj@hotmail.com>
2019-12-17 16:39:26 -0500

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+
+    size = len(my_list)
+    if idx >= size:
+        return my_list
+    elif idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
