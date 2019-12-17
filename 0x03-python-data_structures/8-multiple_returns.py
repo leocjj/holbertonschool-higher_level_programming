9334c8d3cd8fa177adead3ff4ffa0cf5c259937d
leojj <leocj@hotmail.com>
2019-12-17 18:50:16 -0500

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,13 @@
 
 def multiple_returns(sentence):
 
-    return (len(sentence), sentence[0])
+    tup = ()
+    size = len(sentence)
+    if size > 0:
+        first = sentence[0]
+    else:
+        first = ''
+    tup += (size,)
+    tup += (first,)
+
+    return tup
