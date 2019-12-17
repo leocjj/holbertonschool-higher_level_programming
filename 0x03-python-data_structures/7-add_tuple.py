2f9cd010dfc883c7af3c45db33d5cb9f479ab3d7
leojj <leocj@hotmail.com>
2019-12-17 18:41:02 -0500

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,15 +6,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     I, J = len(tuple_a), len(tuple_b)
     ta, tb, tc = tuple_a, tuple_b, ()
     if I == 0:
-        ta = (0,0)
+        ta = (0, 0)
     if I == 1:
-        ta += (0,)
+        ta += (0, )
     if J == 0:
-        tb = (0,0)
+        tb = (0, 0)
     if J == 1:
-        tb += (0,)
+        tb += (0, )
 
     for i in range(2):
         tc += (ta[i] + tb[i],)
-    
+
     return tc
