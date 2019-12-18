0463dbca323481fe7ab15228e9034e5a383839e6
leojj <leocj@hotmail.com>
2019-12-17 19:05:19 -0500

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if my_list == None:
+        return None
+
+    max = my_list[0]
+
+    for i in range(len(my_list)):
+        if my_list[i] > max:
+            max = my_list[i]
+
+    return max
