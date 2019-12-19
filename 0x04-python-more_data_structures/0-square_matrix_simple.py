fb35b44dcff4b9344d4ed292c4509892238ea174
leocjj <leocj@hotmail.com>
2019-12-19 20:57:56 +0000

    Task_0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,5 @@
 
 
 def square_matrix_simple(matrix=[]):
-    a = [[matrix[i][j]**2 for j in range(len(matrix[0]))]
-            for i in range(len(matrix))]
-
+    return ([[matrix[i][j]**2 for j in range(len(matrix[0]))]
+            for i in range(len(matrix))])
