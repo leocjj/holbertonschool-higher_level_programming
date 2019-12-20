2db9e1af1ea3086291bbd52bd23016de5a018789
leojj <leocj@hotmail.com>
2019-12-19 22:13:14 -0500

    Task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+
+    b_dictionary = {}
+
+    for key in list(a_dictionary.keys()):
+        b_dictionary[key] = a_dictionary[key] * 2
+
+    return b_dictionary
