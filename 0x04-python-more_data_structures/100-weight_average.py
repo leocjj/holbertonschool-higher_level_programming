864cc50b6c29e0169aa83c95bd65eb48cb3d62c9
leojj <leocj@hotmail.com>
2019-12-21 15:27:04 -0500

    Task_13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+
+    average = 0
+    counter = 0
+
+    if not my_list:
+        return 0
+
+    print(my_list)
+    for num in my_list:
+        average += num[0]*num[1]
+        counter += num[1]
+    return (average/counter)
