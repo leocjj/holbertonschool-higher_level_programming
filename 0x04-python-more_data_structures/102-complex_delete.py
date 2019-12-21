3a9882041049570357ef9c1fcdf3aeee3d438187
leojj <leocj@hotmail.com>
2019-12-21 16:47:49 -0500

    Task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+
+    if not a_dictionary:
+        return 0
+
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+
+    return a_dictionary
