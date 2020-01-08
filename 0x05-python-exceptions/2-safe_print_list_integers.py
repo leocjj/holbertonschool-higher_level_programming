d2642e1912f6b56ef0149c3e3387c4ad5022e162
leojj <leocj@hotmail.com>
2020-01-08 13:41:43 -0500

    Task 02

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    integers_printed = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            integers_printed += 1 
+        except IndexError:
+            raise
+        except:
+            continue
+
+    print()
+
+    return integers_printed
