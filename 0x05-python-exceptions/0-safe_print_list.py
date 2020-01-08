c452b13c80c3ca87c7b6dc2257dd12f41aa86ef3
leojj <leocj@hotmail.com>
2020-01-08 12:51:28 -0500

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+	chars_printed = 0
+	try:
+		for i in range(x):
+			print(my_list[i], end="")
+			chars_printed += 1 
+		print()
+	except:
+		print()
+
+	return(chars_printed)
+	
