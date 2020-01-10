2887bfdcad5a633ae6a54c277f1b9944d72648ea
Leo CJ <leocj@hotmail.com>
2020-01-09 20:09:22 -0500

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+""""Module to define Square class"""
 
+
+class Square:
+    """"Square class"""
+
+    __init__(self, size=0)
+        """init method"""
+        self.__size = size
