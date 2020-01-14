8891ab0eeb832a5d06d39f1d0892f57eb6894c9d
leocjj <leocj@hotmail.com>
2020-01-14 21:03:12 +0000

    Task_01

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""""Module to define a rectangle class"""
+
+
+class Rectangle:
+    """"Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError('width must be an integer')
+        if height < 0:
+            raise ValueError('width must be >= 0')
+        self.__height = height
