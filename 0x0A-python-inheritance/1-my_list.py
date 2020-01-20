7332f4fb7fb059abc3e7c4a007ef513187f3c157
leocjj <leocj@hotmail.com>
2020-01-20 16:26:30 -0500

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,6 @@
 Public instance method: def print_sorted(self): that prints the list,
 but sorted (ascending sort)
 You can assume that all the elements of the list will be of type int
-You are not allowed to import any module
 """
 
 
