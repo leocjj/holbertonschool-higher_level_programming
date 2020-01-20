548ac50f71ff78755b718fc73f24234ca7c51e9c
leocjj <leocj@hotmail.com>
2020-01-20 16:26:14 -0500

    Task 05

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class BaseGeometry
+
+empty class BaseGeometry.
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry.
+
+    empty class BaseGeometry.
+    """
+    pass
