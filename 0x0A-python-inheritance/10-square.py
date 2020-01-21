f9c85dd96a9c9625ed8ecaf6e245f0eeed66782d
leojj <leocj@hotmail.com>
2020-01-20 22:58:18 -0500

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -22,4 +22,9 @@ class Square(Rectangle):
     class Square.
     """
     def __init__(self, size):
+        """size must be private. No getter or setter
+           size must be a positive integer, validated by integer_validator
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
