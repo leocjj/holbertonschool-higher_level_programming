4fbf5cf12a762865ec18b69b37ee431a8b6a67f2
leojj <leocj@hotmail.com>
2020-01-20 22:11:32 -0500

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for function that adds a new attribute to an object if it’s possible
+
+Raise a TypeError exception, with the message can't add new attribute if the
+object can’t have new attribute
+"""
+
+
+def add_attribute(obj, attribute, value):
+    """Function that adds a new attribute to an object if it’s possible:
+
+    Args:
+        obj: the object add a new attribute.
+        attribute: attribute to add.
+        value: value to set the attribute.
+
+    Returns:
+        None: nothing
+    """
+    for att in dir(obj):
+        if att == "__setattr__":
+            setattr(obj, attribute, value)
+            return
+    raise TypeError("can't add new attribute")
