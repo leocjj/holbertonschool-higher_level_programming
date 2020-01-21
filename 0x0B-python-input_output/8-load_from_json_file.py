6cb8efb1e907a17ed3ab7ce11e2fb2e600af966b
leocjj <leocj@hotmail.com>
2020-01-21 14:41:22 -0500

    Task 08

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module with function that creates an Object from a “JSON file”:
+
+    Prototype: def load_from_json_file(filename):
+    You must use the with statement
+    You don’t need to manage exceptions if the JSON string doesn’t represent an object.
+    You don’t need to manage file permissions / exceptions.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”:
+
+    Args:
+        filename: file to read json representation of my_obj
+
+    Returns:
+        None: nothing
+    """
+    with open(filename) as f:
+        return json.load(f)
