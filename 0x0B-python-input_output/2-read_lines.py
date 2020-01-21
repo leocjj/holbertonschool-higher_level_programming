75295012ac8f0d6f12de9c13eab71408016c7b75
leocjj <leocj@hotmail.com>
2020-01-21 13:30:00 -0500

    Task 02

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module with function that reads n lines of a text file and prints it:
+
+    Prototype: def read_lines(filename="", nb_lines=0):
+    Read the entire file if nb_lines is lower or equal to 0 OR
+        greater or equal to the total number of lines of the file
+    You must use the with statement
+    You don’t need to manage file permission or file doesn't exist exceptions.
+    You are not allowed to import any module
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Reads n lines of a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: the file name to open.
+        nb_lines: number of lines to print
+
+    Returns:
+        int: number of lines of a text file
+    """
+    if nb_lines <= 0:
+        with open(filename) as f:
+            print(f.read(), end='')
+            return
+
+    num_lines = 0
+    with open(filename) as f:
+        for a_line in f:
+            num_lines += 1
+            if num_lines <= nb_lines:
+                print(a_line, end='')
+            else:
+                return
+    return
