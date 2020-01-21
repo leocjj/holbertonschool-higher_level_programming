9ce3dba71541473561bb783b332c78ffcdc6ea65
leojj <leocj@hotmail.com>
2020-01-20 22:44:35 -0500

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""class Rectangle
+
+class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+
+Instantiation with width and height: def __init__(self, width, height):
+    width and height must be private. No getter or setter
+    width and height must be positive integers, validated by integer_validator
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
 """class Square
 
 class Square that inherits from Rectangle (9-rectangle.py):
 """
-
-
-Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
