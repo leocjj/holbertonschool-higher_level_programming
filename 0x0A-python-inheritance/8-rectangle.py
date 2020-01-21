8ec66493196b4aed3ca9371f42c97d34717ec3fe
leojj <leocj@hotmail.com>
2020-01-20 22:44:17 -0500

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """class BaseGeometry
 
+class BaseGeometry
+
+Public instance method: def area(self): that raises an Exception with the
+    message area() is not implemented
+Public instance method: def integer_validator(self, name, value): that
+    validates value: you can assume name is always a string
+if value is not an integer: raise a TypeError exception, with the message
+    <name> must be an integer
+if value is less or equal to 0: raise a ValueError exception with the message
+    <name> must be greater than 0
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+"""class Rectangle
+
 class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
 
 Instantiation with width and height: def __init__(self, width, height):
     width and height must be private. No getter or setter
     width and height must be positive integers, validated by integer_validator
 """
-
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
