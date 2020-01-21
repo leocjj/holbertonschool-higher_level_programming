b4a93151bf74d1ad1d1063591974864877f3bd1d
leojj <leocj@hotmail.com>
2020-01-20 21:16:56 -0500

    Task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int:
+
+    MyInt is a rebel. MyInt has == and != operators inverted
+"""
+
+
+class MyInt(int):
+    """class MyInt.
+
+    MyInt has == and != operators inverted
+    """
+    def __eq__(self, other):
+        """OVERRIDED METHOD FOR x==y calls x.__eq__(y)
+
+        Args:
+            self: the object itself.
+            other: the other object to compare.
+
+        Returns:
+            bool: True if x is different from y
+        """
+        if type(int(self)) != int:
+            exit(1)
+        if type(int(other)) != int:
+            exit(1)
+        if int(self) == int(other):
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+        """OVERRIDED METHOD FOR x!=y calls x.__ne__(y)
+
+        Args:
+            self: the object itself.
+            other: the other object to compare.
+
+        Returns:
+            bool: True if x is different from y
+        """
+        if type(int(self)) != int:
+            exit(1)
+        if type(int(other)) != int:
+            exit(1)
+        if int(self) != int(other):
+            return False
+        else:
+            return True
