f97a24a9d614054526a9b8022cc1b5ce6cf15415
leojj <leocj@hotmail.com>
2020-01-21 23:20:55 -0500

    Task 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module with a function that returns a list of lists
+
+    returns list of lists of integers representing the Pascal’s triangle of n
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
+"""
+
+
+def pascal_triangle(n):
+    pt = []
+    if n <= 0:
+        return []
+    if n == 1:
+        return [1]
+    for i in range(1, n + 1):
+        num = 1
+        row = []
+        for j in range(1, i + 1):
+            row += [num]
+            num = num * (i - j) // j
+        pt += [row]
+    print(pt)
+    return pt
