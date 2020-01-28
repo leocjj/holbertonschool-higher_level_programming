a08b0ce977169f4dad91335ee581138c9c7f7e67
leojj <leocj@hotmail.com>
2020-01-27 21:45:14 -0500

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,4 +31,4 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries:
             return json.dumps(list_dictionaries)
-        return []
+        return '[]'
