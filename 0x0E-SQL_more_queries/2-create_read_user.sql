60d7f6aff9908bddd7311b5e235be895a7440b2c
Leo CJ <53725367+leocjj@users.noreply.github.com>
2020-03-03 22:26:13 -0500

    Task 02

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- List all privileges of the MySQL users user_0d_1 and user_0d_2
--- on your server (in localhost).
+-- creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost;
 SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
