71c9fc04d651edae619336b705cf2e7ce93f1936
Leo CJ <53725367+leocjj@users.noreply.github.com>
2020-03-03 23:20:19 -0500

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 -- Each record should display: <TV Show genre> - <Number of shows linked to this genre>.
-SELECT tv_genres.name FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY genre ORDER BY number_of_shows DESC;
