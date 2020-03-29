180855fe2e7b3f5cb5ca06ee5f53c0362728b1e2
leocjj <leocj@hotmail.com>
2020-03-28 23:44:09 -0500

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    if len(argv) != 4:
-        exit()
-
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
