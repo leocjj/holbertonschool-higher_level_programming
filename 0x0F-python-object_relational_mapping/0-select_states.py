7f6c57feb0e4a6a28822e462f8e11521b38b665f
leocjj <leocj@hotmail.com>
2020-03-28 23:35:52 -0500

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    if len(argv) != 4:
+        exit()
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    value = cur.fetchone()
+    while value != None:
+        print (value)
+        value = cur.fetchone()
+
+    cur.close()
+    db.close()
