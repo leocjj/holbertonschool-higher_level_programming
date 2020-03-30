34485f15ad0acdea2184fd17af1f6e6d54a4086a
leocjj <leocj@hotmail.com>
2020-03-29 23:42:56 -0500

    Create 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+https://auth0.com/blog/sqlalchemy-orm-tutorial-for-python-developers/
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    # generate database schema
+    Base.metadata.create_all(engine)
+
+    # SQLAlchemy ORM session factory bound to this engine
+    Session = sessionmaker(bind=engine)
+    # Create a new session
+    session = Session()
+
+    # Create, add and commit new object to DB.
+    states = session.query(State).filter(State.name.ilike('%a%')).all()
+
+    for state in states:
+        session.delete(state)
+
+    session.commit()
+
+    session.close()
