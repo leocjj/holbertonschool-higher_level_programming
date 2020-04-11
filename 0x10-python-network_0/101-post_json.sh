56108c3fd756e4fefe5018b2e508c6000cf622f9
leocjj <leocj@hotmail.com>
2020-04-11 15:47:44 -0500

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl --header "Content-Type: application/json" --request POST --data @"$2" "$1"
+curl --silence --header "Content-Type: application/json" --request POST --data @"$2" "$1"
