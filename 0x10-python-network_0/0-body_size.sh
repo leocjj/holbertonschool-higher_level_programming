b49a7d7e7e948260158d8a6df2d4e75ebb8b655b
leocjj <leocj@hotmail.com>
2020-04-11 05:57:25 -0500

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | awk '{print $2;}'
+curl --silent --head "$1" | grep Content-Length | awk '{print $2;}'
