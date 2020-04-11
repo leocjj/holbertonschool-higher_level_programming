8e8c00ba9ce1251fcd7188ff75b4f6dc8c4e5e1b
leocjj <leocj@hotmail.com>
2020-04-11 06:52:34 -0500

    Task 4

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl --silent --header "X-HolbertonSchool-User-Id:98" 0.0.0.0:5000/route_5
