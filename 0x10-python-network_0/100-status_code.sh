527ce94b2e6b0b1a26361c2ddc5dc9034e491206
leocjj <leocj@hotmail.com>
2020-04-11 14:05:03 -0500

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl --output /dev/null --silent --write-out "%{http_code}\n" 0.0.0.0:5000
+curl --silent --write-out "%{http_code}\n" "$1" --output /dev/null
