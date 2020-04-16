01172d90fd86d40fb4b53b304a3ad58f7be62173
leocjj <leocj@hotmail.com>
2020-04-16 18:44:59 -0500

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,9 @@ the Github API to display your id
 if __name__ == "__main__":
     import requests
     from sys import argv
-
-    payload = {'key1': 'value1', 'key2': 'value2'}
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     for i in range(10):
-        print(r.json()[i].get('sha'), end=': ')
-        print(r.json()[i].get('commit').get('author').get('name'))
+        commit = r.json()[i]
+        print('{}: {}'.format(commit.get('sha'), commit.get('commit')
+                              .get('author').get('name')))
