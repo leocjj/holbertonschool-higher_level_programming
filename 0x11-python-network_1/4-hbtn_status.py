637758367deb54e0e12de93bfc9c907759640c15
leocjj <leocj@hotmail.com>
2020-04-16 16:09:30 -0500

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL and displays the value
-of the variable X-Request-Id in the response header
+script that fetches https://intranet.hbtn.io/status
 """
 
 
 if __name__ == "__main__":
     import requests
-    from sys import argv
 
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
