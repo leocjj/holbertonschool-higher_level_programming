9b93644667e5f23df675898eb0a20c1b57002182
leocjj <leocj@hotmail.com>
2020-04-16 18:49:20 -0500

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-script that takes your Github credentials (username and password) and uses
-the Github API to display your id
+Use the Github API to list the 10 most recent commits
 """
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
-    for i in range(10):
-        commit = r.json()[i]
+    for commit in r.json()[0:10]:
         print('{}: {}'.format(commit.get('sha'), commit.get('commit')
                               .get('author').get('name')))
