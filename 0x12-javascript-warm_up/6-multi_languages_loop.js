f3ea6e704b8808277d6247b3dd454886128528e1
leocjj <leocj@hotmail.com>
2020-05-05 15:30:17 -0500

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 /*
 Script that prints 3 lines:
-*/
 const messages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 messages.forEach(function (value) { console.log(value); });
+*/
+const messages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
