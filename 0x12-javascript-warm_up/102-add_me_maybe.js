477cbde3e544a566b8647cdd0c5ba8792e72de38
leocjj <leocj@hotmail.com>
2020-05-05 18:28:49 -0500

    Comments updated

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-function that executes x times a function.
+function that increments and calls a function.
 */
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
