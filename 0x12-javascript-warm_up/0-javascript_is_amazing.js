789cb0cac8c048d708ec715d3f8cba3069d2c898
leocjj <leocj@hotmail.com>
2020-05-05 14:25:22 -0500

    Create 0-javascript_is_amazing.js

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+script that takes your Github credentials (username and password) and uses
+the Github API to display your id
+*/
+const myVar = 'Javascript is amazing';
+console.log(myVar);
