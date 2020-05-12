da71a2c1e4cd42ef629fc9f74b9f94e21579256d
leocjj <leocj@hotmail.com>
2020-05-12 15:52:43 -0500

    Create 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+try {
+  const data = fs.readFileSync(process.argv[2]);
+  process.stdout.write(data);
+  // console.log(data);
+  // console.log(data.toString());
+} catch (err) {
+  console.log(err);
+  console.error(err);
+}
+/*
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  process.stdout.write(data);
+  // console.log(data);
+});
+*/
