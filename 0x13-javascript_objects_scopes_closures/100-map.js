edc0785f9f7bec1f6f1aabf21e04a9a7e7c0b236
leocjj <leocj@hotmail.com>
2020-05-11 23:53:58 -0500

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const listMaped = list.map((x, i) => x * i);
+const listMaped = list.map(function (x, i) { return (x * i); });
 console.log(list);
 console.log(listMaped);
