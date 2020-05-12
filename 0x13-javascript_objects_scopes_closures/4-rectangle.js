85405d9414d59689b8c0a3a50e247e4acab6d2ec
leocjj <leocj@hotmail.com>
2020-05-11 22:52:14 -0500

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+/*
+Rectangle.prototype.print = function () {
+  for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+  }
+};
+*/
+
+module.exports = Rectangle;
