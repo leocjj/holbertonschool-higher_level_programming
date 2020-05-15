e6a1f8e4a9d2b3fe31e6582109fe9bb5dcff087c
leocjj <leocj@hotmail.com>
2020-05-15 00:10:30 -0500

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-$.getJSON('https://swapi.co/api/films/?format=json', function (resp) {
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (resp) {
   resp.results.forEach(function (film) {
     $('#list_movies').append('<li>' + film.title + '</li>');
   });
